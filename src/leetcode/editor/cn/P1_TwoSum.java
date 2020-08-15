76f4891e6ca1f1f46fce0f2d210e576f9e2d7622
z-dk <zhudengkui0000@126.com>
2020-08-15 16:38:49 +0800

    Initial commit

---
+++ src/leetcode/editor/cn/P1_TwoSum.java
@@ -0,0 +1,41 @@
+//给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。 
+//
+// 你可以假设每种输入只会对应一个答案。但是，数组中同一个元素不能使用两遍。 
+//
+// 
+//
+// 示例: 
+//
+// 给定 nums = [2, 7, 11, 15], target = 9
+//
+//因为 nums[0] + nums[1] = 2 + 7 = 9
+//所以返回 [0, 1]
+// 
+// Related Topics 数组 哈希表 
+// 👍 8897 👎 0
+
+
+package leetcode.editor.cn;
+
+//两数之和
+
+public class P1_TwoSum{
+    public static void main(String[] args) {
+        //测试代码
+        Solution solution = new P1_TwoSum().new Solution();
+    }
+//力扣代码
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int[] result = new int[2];
+        for (int i=0;i<nums.length;i++){
+            
+        }
+        return result;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
+
